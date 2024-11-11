44512e306357896c18a0e5ced2da33ebca6da352
Katy Long <katy.long@wearecodenation.com>
2024-11-11 12:31:12 +0000

    loop notes added

---
+++ g_for_loop_notes.py
@@ -0,0 +1,20 @@
+# website_users = ["Charles", "Paul", "Ron", "Sanna", "Crystal", "Prathamm", "Joe"]
+
+# for user in website_users:
+#     print(f"\nDear {user}!")
+#     print("Thanks for using Cloud Admirers monthly")
+#     print("It is now time to change your password")
+#     print("Thanks - Klong\n")
+
+# for i in range(10):
+#     print(i)
+
+# for i in range(5,11):
+#     print(i)
+
+total = 0
+
+for i in range(1,101):
+    total +=i
+
+print(total)
---
+++ h_while_loop.py
@@ -0,0 +1,7 @@
+answer = input("Who ordered the cortado?")
+
+while answer !="Alex":
+    print("incorrect")
+    answer = input("Who ordered the cortado?")
+else:
+    print("Enjoy your coffee!")
