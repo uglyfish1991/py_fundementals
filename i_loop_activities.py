1de5312f2bccf1aeea85e59f94d9dcfdbb502def
Katy Long <katy.long@wearecodenation.com>
2024-11-11 13:56:14 +0000

    loop answers added

---
+++ i_loop_activities.py
@@ -0,0 +1,21 @@
+#------ Activity 1 A -------------
+
+# for i in range(1, 14):
+#     print("Hello World")
+
+# for i in range(13):
+#     print("hello world")
+
+# ------ Activity 1 B -------------
+
+# count = 0
+# while count < 13:
+#     print("hello world")
+#     count += 1
+
+# ------ Activity 2 -------------
+
+for i in range(1,13):
+    for j in range(1,13):
+        print(f"{i} x {j} = {i*j}")
+    print()
